00a16b2d107ea418f826911a4d1b6c796f614e9e
Kousik Ambani <rikousik@gmail.com>
2023-05-26 01:01:50 +0600

    auth + task working

--- wiki_api/urls.py
+++ wiki_api/urls.py
@@ -1,7 +1,7 @@
-"""mysite URL Configuration
+"""wiki_api URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.0/topics/http/urls/
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -14,9 +14,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import path,include
 
+
+from django.conf import settings
+from django.conf.urls.static import static
 urlpatterns = [
+    path('auth/', include('dj_rest_auth.urls')),
+    path('auth/registration/', include('dj_rest_auth.registration.urls')),
+
+   # path('', IndexTemplateView.as_view(), name="home"),
     path('admin/', admin.site.urls),
     path("api/core/",include('core.urls')),
-]
+    # path("api/internship/",include('internship.urls')),
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
